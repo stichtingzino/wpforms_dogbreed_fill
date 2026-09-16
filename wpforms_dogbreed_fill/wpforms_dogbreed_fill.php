b01794e9a323b44ab8db4bf7174990cf2b5a2c90
markzn01 <mark@stichtingzino.nl>
2026-09-16 17:05:29 +0200

    Fix header for github updater

--- wpforms_dogbreed_fill/wpforms_dogbreed_fill.php
+++ wpforms_dogbreed_fill/wpforms_dogbreed_fill.php
@@ -9,6 +9,8 @@
  * Plugin Name: WPF Dog Breed Fill
  * Plugin URI: https://github.com/stichtingzino/wpforms_dogbreed_fill
  * GitHub Plugin URI: https://github.com/stichtingzino/wpforms_dogbreed_fill
+ * Release Asset: true
+ * Primary Branch: main
  * Description: A plugin to fill dog breed options in WPForms.
  * Version: 0.1.4-rc3
  * Author: Mark Blom
